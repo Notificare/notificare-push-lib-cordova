e110e083fc63e3ee95d5285f7be9e10e658d9e15
silentjohnny <joris@notifica.re>
2014-09-18 00:48:47 +0200

    Initial Commit

---
+++ src/ios/cordova/cordova/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cordova
+//
+//  Created by Joris Verbogt on 18/09/14.
+//  Copyright (c) 2014 Notificare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ src/ios/cordova/cordova/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cordova
+//
+//  Created by Joris Verbogt on 18/09/14.
+//  Copyright (c) 2014 Notificare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
